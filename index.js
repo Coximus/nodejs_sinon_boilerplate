0005c17a28b9c228507a1dfe859e56be6a71e5e1
paul Alcock <paul.alcock@skybettingandgaming.com>
2016-10-05 09:34:29 +0100

    Cleared up index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-var Plugwise = require('./Plugwise'),
-    plugwise;
+var app = require('./src/app');
 
-var connectToPlugwise = function(serial, callback) {
-    plugwise = Plugwise.getInstance();
-    plugwise.connect(serial, function(error) {
-        if(error) {
-            return callback(error);
-        }
-        return callback(null, plugwise);
-    });
-}
-
-var callSwitchPlug = function(plugId, desiredState, callback) {
-    plugwise.switchPlug(plugId, desiredState, callback);
-}
-
-module.exports = {
-    connect: function(serial, callback) {
-        connectToPlugwise(serial, function(error, plugwise) {
-            if(callback) {
-                callback(error, plugwise);
-            }
-        });
-    },
-    switchPlug: function(plugId, desiredState, callback) {
-        if(plugwise && plugwise.connected) {
-            return callSwitchPlug(plugId, desiredState, callback);
-        }
-        return callback('Plugwise not connected');
-    }
-};
+app.start();
